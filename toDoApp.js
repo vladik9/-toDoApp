e555cd627d1de9bb9024a0c8b52cebe92b26bf92
vladik9 <v.cornici@gmail.com>
2021-07-07 17:50:18 +0000

    adding style and behaivor for app

--- toDoApp.js
+++ toDoApp.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("Public"));
+let userItems = ["To buy mill", "To take and wash my clouths", "To get a job"];
 /// use of ejs
 app.listen(8080, () => {
   console.log("listening on port 8080");
@@ -11,17 +12,18 @@ app.listen(8080, () => {
 
 app.set("view engine", "ejs");
 
-var today = new Date();
-
-var options = { weekday: "long", day: "numeric", month: "long" };
-
-var day = today.toLocaleDateString("en-US", options);
-console.log(day);
 app.get("/", (req, res) => {
-  res.render("list", { CurrentDay: day });
+  var today = new Date();
+  var options = { weekday: "long", day: "numeric", month: "long" };
+  var day = today.toLocaleDateString("en-US", options);
+
+  console.log(day);
+  res.render("list", { CurrentDay: day, newItems: userItems });
 });
 
 app.post("/", (req, res) => {
   var userItem = req.body.userListItem;
-  console.log(userItem);
+  userItems.push(userItem);
+
+  res.redirect("/");
 });
