fd41c664025366b3ca5d83d45b86884d1fc275ae
vladik9 <v.cornici@gmail.com>
2021-07-07 13:54:21 +0000

    adding ejs and modifyed js file

--- toDoApp.js
+++ toDoApp.js
@@ -4,11 +4,24 @@ const express = require("express");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("Public"));
-
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+/// use of ejs
+app.listen(8080, () => {
+  console.log("listening on port 8080");
 });
 
-app.listen(3000, function () {
-  console.log("listening on port 3000");
+app.set("view engine", "ejs");
+
+var today = new Date();
+
+var options = { weekday: "long", day: "numeric", month: "long" };
+
+var day = today.toLocaleDateString("en-US", options);
+console.log(day);
+app.get("/", (req, res) => {
+  res.render("list", { CurrentDay: day });
+});
+
+app.post("/", (req, res) => {
+  var userItem = req.body.userListItem;
+  console.log(userItem);
 });
